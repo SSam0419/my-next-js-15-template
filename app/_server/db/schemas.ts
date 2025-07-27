83262380c97a0ba5d2de71c668bd6f65a28b9c65
SSam0419 <leemingchun419@gmail.com>
2025-07-27 22:14:16 +0800

    add landing page

--- app/_server/db/schemas.ts
+++ app/_server/db/schemas.ts
@@ -1,17 +1,25 @@
 import {
-  integer,
-  primaryKey,
   sqliteTable,
-  text,
+  text
 } from "drizzle-orm/sqlite-core";
 import { nanoid } from "nanoid";
 
-export const helloTable = sqliteTable("hello_world", {
+
+
+export const tagTable = sqliteTable("tag", {
+  id: text("id").notNull().primaryKey().$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  categoryId: text("category_id").references(()=>tagCategoryTable.id).notNull()
+  })
+
+export const tagCategoryTable = sqliteTable("tag_category", {
   id: text("id")
     .notNull()
     .primaryKey()
     .$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
   createdAt: text("created_at")
     .notNull()
     .$defaultFn(() => new Date().toISOString()),
-});
+})
